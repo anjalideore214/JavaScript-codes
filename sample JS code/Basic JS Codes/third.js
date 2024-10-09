2154d1b5127c220bb1bdf59db8ac5434d7410de1
username <anjalideore214@gmail.com>
2024-10-09 18:40:37 +0530

    commited

---
+++ sample JS code/Basic JS Codes/third.js
@@ -0,0 +1,6 @@
+// This gives popup when enter in website
+alert("Hello to this Page");
+// This changes the background color of body
+document.body.style.backgroundColor='Blue';
+// This Enter the New value for second ID's element
+document.getElementById('second').innerHTML="Hello to this second div and new paragraph";
---
+++ sample JS code/JS Code using HTML/JS Variables/Var1.js
@@ -0,0 +1,21 @@
+// Declaration of variable using var keyword
+var num = 10;
+
+// Global Variables
+var num1 = 30;
+function a() {
+    // Body of function
+}
+// Global Variables can be declare anywhere in code
+
+// Local Variables
+function b() {
+    var num2 = 20;
+    // Body of function
+}
+// Local Variables are declare inside the function
+
+// JS code can used with HTML code
+// <script></script> tag is used JS code 
+
+
